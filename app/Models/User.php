85cb9753995d0c21a66f102021238d062cb9c661
ThiagoVasque <thiagonvasque@gmail.com>
2026-01-27 18:58:32 -0300

    Fix: permitindo acesso ao painel do Filament

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,42 +2,35 @@
 
 namespace App\Models;
 
-// use Illuminate\Contracts\Auth\MustVerifyEmail;
+use Filament\Models\Contracts\FilamentUser; // IMPORTANTE
+use Filament\Panel; // IMPORTANTE
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
-class User extends Authenticatable
+class User extends Authenticatable implements FilamentUser // IMPORTANTE: Adicionado implements
 {
     /** @use HasFactory<\Database\Factories\UserFactory> */
     use HasFactory, Notifiable;
 
-    /**
-     * The attributes that are mass assignable.
-     *
-     * @var list<string>
-     */
     protected $fillable = [
         'name',
         'email',
         'password',
     ];
 
-    /**
-     * The attributes that should be hidden for serialization.
-     *
-     * @var list<string>
-     */
     protected $hidden = [
         'password',
         'remember_token',
     ];
 
-    /**
-     * Get the attributes that should be cast.
-     *
-     * @return array<string, string>
-     */
+    // IMPORTANTE: Este método é o que mata o erro 403
+    public function canAccessPanel(Panel $panel): bool
+    {
+        // Aqui estamos liberando para qualquer usuário cadastrado logar.
+        return true; 
+    }
+
     protected function casts(): array
     {
         return [
